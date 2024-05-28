6222b0ccbea8af3c3aa1e477053bd724411c56b5
AllanKariuki <alankariuki24@gmail.com>
2024-05-28 11:42:37 +0300

    feat: initialization and switch to authenticate

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,11 @@ app.get("/", (request: Request, response: Response) => {
 
 app.use("/", usersRouter);
 
-sequelize.sync().then(() => {
-     app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`)
-    })
-}).catch((error: Error) => {
-    console.log('Unable to connect the database', error);
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}).catch (error => {
+    console.error('Unable to connect to the database:', error);
 });
